891ea91a2c6b5b468c8836c9956207c503c7dc95
YRim99 <alsrod2017@naver.com>
2021-08-04 12:02:31 +0900

    색 바꿈 & SplashActivity

---
+++ app/src/main/java/com/example/login/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+
+@Suppress("DEPRECATION")
+class SplashActivity : AppCompatActivity() {
+    val SPLASH_VIEW_TIME: Long = 3000 //2초간 스플래시 화면을 보여줌 (ms)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler().postDelayed({ //delay를 위한 handler
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_VIEW_TIME)
+    }
+}
