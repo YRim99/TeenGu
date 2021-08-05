b4d42ea111609654bc5f60acdf98c13e167fc775
YRim99 <alsrod2017@naver.com>
2021-08-05 16:15:03 +0900

    게시판 디자인 수정

--- app/src/main/java/com/example/login/Board_marketActivity.kt
+++ app/src/main/java/com/example/login/Board_marketActivity.kt
@@ -114,7 +114,6 @@ class Board_marketActivity : AppCompatActivity() {
             //마이페이지로 이동
             R.id.action_mypage -> {
                 val intent = Intent(this, MypageActivity::class.java)
-
                 intent.putExtra("intent_userid", login_id)
                 startActivity(intent)
 
